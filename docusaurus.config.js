7fca5ac98c3314c524c1b9e0581ecf5e3ade9390
petar-cvit <petar.cvitanovic@gmail.com>
2023-05-20 18:15:37 +0200

    trigger

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,7 +34,7 @@ const config = {
 
   presets: [
     [
-      'classic',
+      '@docusaurus/preset-classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
