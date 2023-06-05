1481887dd9ae555d5722043024df33f602a4e220
Petar Cvitanović <petar.cvitanovic@superbet.com>
2023-06-05 13:15:22 +0200

    EIT supporter

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,10 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import helmsman from '/static/img/cyclops_helmsman.png';
-import nuqleus from '/static/img/nuqleus_logo.png';
+import nuqleus from '/static/img/nuqleus_landscape.png';
+// import nuqleus from '/static/img/nuqleus_logo.png';
+import EITDigital from '/static/img/EIT_digital.png';
+import EIT from '/static/img/EIT.png';
 
 import styles from './index.module.css';
 import Comparison from "../components/Comparison";
@@ -36,10 +39,27 @@ function HomepageHeader() {
                     </Link>
                 </div>
                 <div className={styles.support}>
-                    <p style={{padding: 0, margin: 0}}>Supported by</p>
-                    <Link to="https://nuqleus.io/">
-                        <img src={nuqleus}/>
-                    </Link>
+                    <p style={{padding: 0, margin: 0, paddingBottom: '5px'}}>Supported by</p>
+                    <ul>
+                        <li>
+                            <Link to="https://nuqleus.io/">
+                                <img style={{width: 'auto', height: '32px'}} src={nuqleus}/>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="https://www.eitdigital.eu/">
+                                <img style={{width: 'auto', height: '64px'}} src={EIT}/>
+                            </Link>
+                        </li>
+                    </ul>
+                    {/*<div>*/}
+                    {/*    <Link to="https://nuqleus.io/">*/}
+                    {/*        <img style={{width: 'auto', height: '32px'}} src={nuqleus}/>*/}
+                    {/*    </Link>*/}
+                    {/*    <Link to="https://nuqleus.io/">*/}
+                    {/*        <img style={{width: 'auto', height: '64px'}} src={EIT}/>*/}
+                    {/*    </Link>*/}
+                    {/*</div>*/}
                 </div>
             </div>
         </header>
